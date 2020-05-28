29ed560d10e614f7a0a29cb1dad4079f06fbe809
zhang <xiaolinzi95_27@163.com>
2020-05-28 15:38:08 +0800

    first commit

---
+++ fegin-hystrix/fegin-hystrix-server/src/main/java/com/study/xiaolinzi/FeginHystrixServerApplication.java
@@ -0,0 +1,13 @@
+package com.study.xiaolinzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeginHystrixServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeginHystrixServerApplication.class, args);
+	}
+
+}
